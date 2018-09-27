de87a41d764e11fc96fca876ab9c25c34d6ddfc7
ludeeus <joasoe@gmail.com>
2018-09-27 23:31:42 +0200

    linter...

--- pyupdate/__init__.py
+++ pyupdate/__init__.py
@@ -5,6 +5,7 @@ This code is released under the terms of the MIT license. See the LICENSE
 file for more details.
 """
 
+
 def first_function():
     """First function."""
     print('pyupdate - A python module to update stuff.')
