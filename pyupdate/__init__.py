f5605677ac03f49817ceb0c464c683c2840d7700
ludeeus <joasoe@gmail.com>
2018-09-27 23:28:44 +0200

    added new function

--- pyupdate/__init__.py
+++ pyupdate/__init__.py
@@ -1,8 +1,10 @@
 """
-A python module update stuff.
+A python module to update stuff.
 
 This code is released under the terms of the MIT license. See the LICENSE
 file for more details.
 """
 
-print('pyupdate - A python module update stuff.')
+def first_function():
+    """First function."""
+    print('pyupdate - A python module to update stuff.')
