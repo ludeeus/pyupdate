c5e6511718067f23a6f16111a7738626fb7d268f
ludeeus <joasoe@gmail.com>
2018-09-28 20:25:12 +0200

    More fixes

--- pyupdate/ha_custom/custom_components.py
+++ pyupdate/ha_custom/custom_components.py
@@ -6,7 +6,6 @@ from requests import RequestException
 from pyupdate.ha_custom import common
 
 
-@staticmethod
 def _normalize_path(path):
     path = path.replace('/', os.path.sep)\
         .replace('\\', os.path.sep)
