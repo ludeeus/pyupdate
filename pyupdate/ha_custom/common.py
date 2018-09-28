446af43d7e5dac38bf66e6d769a342eb8ef39119
ludeeus <joasoe@gmail.com>
2018-09-28 19:01:30 +0200

    Adde logic for components

--- pyupdate/ha_custom/common.py
+++ pyupdate/ha_custom/common.py
@@ -1,5 +1,6 @@
 """Logic to handle common functions."""
-
+import os
+import requests
 
 def get_default_repos():
     """Return default repos."""
@@ -7,3 +8,46 @@ def get_default_repos():
     cards = git_base + 'custom-cards/information/master/repos.json'
     components = git_base + 'custom-components/information/master/repos.json'
     return [cards, components]
+
+
+def get_repo_data(resource, extra_repos=None):
+    """Update the data about components."""
+    if resource == 'card':
+        resource = 0
+    elif resource == 'component':
+        resource = 1
+    repos = []
+    for repo in get_default_repos()[resource]:
+        repos.append(repo)
+    if extra_repos is not None:
+        for repo in extra_repos:
+            repos.append(repo)
+    return repos
+
+
+def check_local_premissions(file):
+    """Check premissions of a file."""
+    return os.access(file, os.W_OK)
+
+
+def check_remote_access(file):
+    """Check access to remote file."""
+    test_remote_file = requests.get(file)
+    return bool(test_remote_file.status_code == 200)
+
+
+def download_file(local_file, remote_file):
+    """Download a file."""
+    if check_local_premissions(local_file):
+        if check_remote_access(remote_file):
+            with open(local_file, 'wb') as file:
+                file.write(requests.get(file).content)
+            file.close()
+            retrun_value = True
+        else:
+            print('Remote file not accessable.')
+            retrun_value = False
+    else:
+        print('local file not writable.')
+        retrun_value = False
+    return retrun_value
--- pyupdate/ha_custom/custom_components.py
+++ pyupdate/ha_custom/custom_components.py
@@ -1,1 +1,114 @@
 """Logic to handle custom_components."""
+import os
+import re
+import requests
+from requests import RequestException
+from pyupdate.ha_custom import common
+
+
+@staticmethod
+def _normalize_path(path):
+    path = path.replace('/', os.path.sep)\
+        .replace('\\', os.path.sep)
+
+    if path.startswith(os.path.sep):
+        path = path[1:]
+
+    return path
+
+
+def get_info_all_components():
+    """Return all remote info if any."""
+    remote_info = {}
+    for url in common.get_repo_data('component'):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                for name, component in response.json().items():
+                    try:
+                        component = [
+                            name,
+                            component['version'],
+                            _normalize_path(
+                                component['local_location']),
+                            component['remote_location'],
+                            component['visit_repo'],
+                            component['changelog']
+                        ]
+                        remote_info[name] = component
+                    except KeyError:
+                        print('Could not get remote info for ' + name)
+        except RequestException:
+            print('Could not get remote info for ' + url)
+    return remote_info
+
+
+
+def get_sensor_data():
+    """Get sensor data."""
+    components = get_info_all_components()
+    cahce_data = {}
+    cahce_data['domain'] = 'custom_components'
+    cahce_data['has_update'] = []
+    count_updateable = 0
+    if components:
+        for name, component in components.items():
+            remote_version = component[1]
+            local_version = get_local_version(component[2])
+            has_update = (remote_version and
+                          remote_version != local_version)
+            not_local = (remote_version and not local_version)
+            if has_update and not not_local:
+                count_updateable = count_updateable + 1
+                cahce_data['has_update'].append(name)
+            cahce_data[name] = {
+                "local": local_version,
+                "remote": remote_version,
+                "has_update": has_update,
+                "not_local": not_local,
+                "repo": component[4],
+                "change_log": component[5],
+            }
+    return [cahce_data, count_updateable]
+
+
+def update_all(base_dir):
+    """Update all components."""
+    updates = get_sensor_data()[0]['has_update']
+    if updates is not None:
+        for name in updates:
+            upgrade_single(base_dir, name)
+
+
+def upgrade_single(base_dir, name):
+    """Update one component."""
+    print('Starting upgrade for ' + name)
+    remote_info = get_sensor_data()[0][name]
+    remote_file = remote_info[3]
+    local_file = os.path.join(base_dir, remote_info[2])
+    common.download_file(local_file, remote_file)
+
+
+def install(base_dir, name):
+    """Install single component."""
+    if name in get_sensor_data()[0]:
+        if '.' in name:
+            component = str(name).split('.')[0]
+            if not os.path.isdir(component):
+                path = base_dir + '/custom_components/' + component
+                os.mkdir(path)
+        upgrade_single(base_dir, name)
+
+
+def get_local_version(local_path):
+    """Return the local version if any."""
+    if os.path.isfile(local_path):
+        with open(local_path, 'r') as local:
+            pattern = re.compile(r"^__version__\s*=\s*['\"](.*)['\"]$")
+            for line in local.readlines():
+                matcher = pattern.match(line)
+                if matcher:
+                    return_value = matcher.group(1)
+                else:
+                    return_value = False
+    return return_value
