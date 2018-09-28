40aab65f37b685c4ca8969726853680c5d049e8b
ludeeus <joasoe@gmail.com>
2018-09-28 21:02:54 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="pyupdate",
-    version='0.0.4',
+    version='0.0.5',
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="A python module to update stuff.",
