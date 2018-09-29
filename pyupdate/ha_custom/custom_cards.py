1354d3e61ec262cb9c9288e7f16babc9e3bdc39d
ludeeus <joasoe@gmail.com>
2018-09-29 17:53:24 +0200

    wrong dir

--- pyupdate/ha_custom/custom_cards.py
+++ pyupdate/ha_custom/custom_cards.py
@@ -125,14 +125,14 @@ def get_card_dir(base_dir, name):
                     card_dir = base_dir + '/lovelace/' + card[0]
                     break
                 else:
-                    card_dir = base_dir + '/lovelace/' + name + '.js'
+                    card_dir = base_dir + '/lovelace/'
             else:
                 if '/' + name + '.js' in line:
                     card = line.split(': ')[1].split(name + '.js')
                     card_dir = base_dir + card[0].replace("local", "www")
                     break
                 else:
-                    card_dir = base_dir + '/www/' + name + '.js'
+                    card_dir = base_dir + '/www/'
     return card_dir
 
 
