3807fe2c3d22b40fce4c2b986f1cee0adbdf3771
Joakim Sørensen <joasoe@gmail.com>
2019-02-18 10:20:37 +0100

    Restructure (#4)
    * travis update
    
    * initial restructure
    
    * some async updates
    
    * More async
    
    * finish cards async
    
    * async pythonscript
    
    * async comp
    
    * fix for repos
    
    * set remote info
    
    * move line
    
    * fix normalize path
    
    * Fix issue in upgrade
    
    * cleanup
    
    * force dir refresh on startup

--- pyupdate/ha_custom/common.py
+++ pyupdate/ha_custom/common.py
@@ -9,7 +9,7 @@ import requests
 LOGGER = logging.getLogger(__name__)
 
 
-def get_default_repos():
+async def get_default_repos():
     """Return default repos."""
     git_base = 'https://raw.githubusercontent.com/'
     card = [git_base + 'custom-cards/information/master/repos.json']
@@ -20,10 +20,11 @@ def get_default_repos():
             'python_script': python_script}
 
 
-def get_repo_data(resource, extra_repos=None):
+async def get_repo_data(resource, extra_repos=None):
     """Update the data about components."""
     repos = []
-    default_repos = get_default_repos()[resource]
+    default_repos = await get_default_repos()
+    default_repos = default_repos[resource]
     if None not in default_repos:
         for repo in default_repos:
             repos.append(str(repo))
@@ -37,23 +38,23 @@ def get_repo_data(resource, extra_repos=None):
     return repos
 
 
-def check_local_premissions(file):
+async def check_local_premissions(file):
     """Check premissions of a file."""
     dirpath = os.path.dirname(file)
     return os.access(dirpath, os.W_OK)
 
 
-def check_remote_access(file):
+async def check_remote_access(file):
     """Check access to remote file."""
     test_remote_file = requests.get(file)
     return bool(test_remote_file.status_code == 200)
 
 
-def download_file(local_file, remote_file):
+async def download_file(local_file, remote_file):
     """Download a file."""
     LOGGER.debug("Downloading '%s' to '%s'", remote_file, local_file)
-    if check_local_premissions(local_file):
-        if check_remote_access(remote_file):
+    if await check_local_premissions(local_file):
+        if await check_remote_access(remote_file):
             with open(local_file, 'wb') as file:
                 file.write(requests.get(remote_file).content)
             file.close()
@@ -67,17 +68,15 @@ def download_file(local_file, remote_file):
     return retrun_value
 
 
-def normalize_path(path):
+async def normalize_path(path):
     """Normalize the path."""
     path = path.replace('/', os.path.sep).replace('\\', os.path.sep)
-
     if path.startswith(os.path.sep):
         path = path[1:]
-
     return path
 
 
-def replace_all(file, search, replace):
+async def replace_all(file, search, replace):
     """Replace all occupancies of search in file."""
     LOGGER.debug('Replacing all "%s" with "%s" in file "%s"', search,
                  replace, file)
@@ -87,11 +86,7 @@ def replace_all(file, search, replace):
         sys.stdout.write(line)
 
 
-def update(package):
+async def update(package):
     """Update a pip package."""
-    subprocess.call([sys.executable,
-                     "-m",
-                     "pip",
-                     "install",
-                     "--upgrade",
-                     package])
+    await subprocess.call(
+        [sys.executable, "-m", "pip", "install", "--upgrade", package])
