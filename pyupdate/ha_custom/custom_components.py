2db4eb1411fc4e19693ecf19dbbed238d760a050
ludeeus <joasoe@gmail.com>
2019-02-24 20:26:30 +0100

    lining

--- pyupdate/ha_custom/custom_components.py
+++ pyupdate/ha_custom/custom_components.py
@@ -60,7 +60,7 @@ class CustomComponents():
         if components:
             for name, component in components.items():
                 remote_version = component['version']
-                local_file = self.base_dir + '/' + str(component['local_location'])
+                local_file = self.base_dir + str(component['local_location'])
                 local_version = await self.get_local_version(local_file)
                 has_update = (
                     remote_version and remote_version != local_version)
@@ -118,7 +118,7 @@ class CustomComponents():
             if '.' in name:
                 remove = comppath.split('/')[-1]
                 path = comppath.split(remove)[0]
-            elif '__init__.py' == comppath.split('/')[-1]:
+            elif comppath.split('/')[-1] == '__init__.py':
                 path = comppath.split('__init__')[0]
             await self.log.debug('install', path)
             if path is not None:
@@ -177,14 +177,17 @@ class CustomComponents():
         await self.log.debug('downlaod_component_resources', 'Started')
         componentdata = await self.component_data(name)
         iscomponent = False
-        if '__init__.py' == componentdata['local_location'].split('/')[-1]:
+        if componentdata['local_location'].split('/')[-1] == '__init__.py':
             iscomponent = True
         if iscomponent:
             resources = componentdata.get('resources', [])
             await self.log.debug('downlaod_component_resources', resources)
             for resource in resources:
-                target = self.base_dir + componentdata['local_location'].split('__init__')[0]
+                target = self.base_dir + componentdata['local_location']
+                target = target.split('__init__')[0]
                 target = "{}{}".format(target, resource.split('/')[-1])
-                await self.log.debug('downlaod_component_resources', 'resource: ' + resource)
-                await self.log.debug('downlaod_component_resources', 'target: ' + target)
+                await self.log.debug(
+                    'downlaod_component_resources', 'resource: ' + resource)
+                await self.log.debug(
+                    'downlaod_component_resources', 'target: ' + target)
                 await common.download_file(target, resource)
